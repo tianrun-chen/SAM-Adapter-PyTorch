ccc66900a773ad0ab2711ebb8ddd062e99c749ac
Tianrun @ KOKONI 3D <126600557+tianrun-chen@users.noreply.github.com>
2023-04-21 17:50:53 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -9,19 +9,15 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 import datasets
 import models
 import utils
-from test import eval_psnr
-
+from statistics import mean
 import torch
 import torch.distributed as dist
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3,4,5,6,7'
 torch.distributed.init_process_group(backend='nccl')
 local_rank = torch.distributed.get_rank()
-rank = int(os.environ["RANK"])
-print(f"[{os.getpid()}] (rank = {rank}, local_rank = {local_rank}) train worker starting...")
-
 torch.cuda.set_device(local_rank)
 device = torch.device("cuda", local_rank)
+
 
 def make_data_loader(spec, tag=''):
     if spec is None:
@@ -29,12 +25,12 @@ def make_data_loader(spec, tag=''):
 
     dataset = datasets.make(spec['dataset'])
     dataset = datasets.make(spec['wrapper'], args={'dataset': dataset})
+    if local_rank == 0:
+        log('{} dataset: size={}'.format(tag, len(dataset)))
+        for k, v in dataset[0].items():
+            log('  {}: shape={}'.format(k, tuple(v.shape)))
 
-    log('{} dataset: size={}'.format(tag, len(dataset)))
-    for k, v in dataset[0].items():
-        log('  {}: shape={}'.format(k, tuple(v.shape)))
-
-    sampler = torch.utils.data.distributed.DistributedSampler(dataset, shuffle=True)
+    sampler = torch.utils.data.distributed.DistributedSampler(dataset)
     loader = DataLoader(dataset, batch_size=spec['batch_size'],
         shuffle=False, num_workers=8, pin_memory=True, sampler=sampler)
     return loader
@@ -44,6 +40,57 @@ def make_data_loaders():
     train_loader = make_data_loader(config.get('train_dataset'), tag='train')
     val_loader = make_data_loader(config.get('val_dataset'), tag='val')
     return train_loader, val_loader
+
+
+def eval_psnr(loader, model, eval_type=None):
+    model.eval()
+
+    if eval_type == 'f1':
+        metric_fn = utils.calc_f1
+        metric1, metric2, metric3, metric4 = 'f1', 'auc', 'none', 'none'
+    elif eval_type == 'fmeasure':
+        metric_fn = utils.calc_fmeasure
+        metric1, metric2, metric3, metric4 = 'f_mea', 'mae', 'none', 'none'
+    elif eval_type == 'ber':
+        metric_fn = utils.calc_ber
+        metric1, metric2, metric3, metric4 = 'shadow', 'non_shadow', 'ber', 'none'
+    elif eval_type == 'cod':
+        metric_fn = utils.calc_cod
+        metric1, metric2, metric3, metric4 = 'sm', 'em', 'wfm', 'mae'
+
+    if local_rank == 0:
+        pbar = tqdm(total=len(loader), leave=False, desc='val')
+    else:
+        pbar = None
+
+    pred_list = []
+    gt_list = []
+    for batch in loader:
+        for k, v in batch.items():
+            batch[k] = v.cuda()
+
+        inp = batch['inp']
+
+        pred = torch.sigmoid(model.infer(inp))
+
+        batch_pred = [torch.zeros_like(pred) for _ in range(dist.get_world_size())]
+        batch_gt = [torch.zeros_like(batch['gt']) for _ in range(dist.get_world_size())]
+
+        dist.all_gather(batch_pred, pred)
+        pred_list.extend(batch_pred)
+        dist.all_gather(batch_gt, batch['gt'])
+        gt_list.extend(batch_gt)
+        if pbar is not None:
+            pbar.update(1)
+
+    if pbar is not None:
+        pbar.close()
+
+    pred_list = torch.cat(pred_list, 1)
+    gt_list = torch.cat(gt_list, 1)
+    result1, result2, result3, result4 = metric_fn(pred_list, gt_list)
+
+    return result1, result2, result3, result4, metric1, metric2, metric3, metric4
 
 
 def prepare_training():
@@ -59,31 +106,42 @@ def prepare_training():
         epoch_start = 1
     max_epoch = config.get('epoch_max')
     lr_scheduler = CosineAnnealingLR(optimizer, max_epoch, eta_min=config.get('lr_min'))
-    log('model: #params={}'.format(utils.compute_num_params(model, text=True)))
+    if local_rank == 0:
+        log('model: #params={}'.format(utils.compute_num_params(model, text=True)))
     return model, optimizer, epoch_start, lr_scheduler
+
 
 def train(train_loader, model):
     model.train()
-    train_loss_G = utils.Averager()
 
-    for batch in tqdm(train_loader, leave=False, desc='train'):
+    if local_rank == 0:
+        pbar = tqdm(total=len(train_loader), leave=False, desc='train')
+    else:
+        pbar = None
+
+    loss_list = []
+    for batch in train_loader:
         for k, v in batch.items():
-            # batch[k] = v.cuda()
             batch[k] = v.to(device)
-
         inp = batch['inp']
         gt = batch['gt']
-
         model.set_input(inp, gt)
         model.optimize_parameters()
+        batch_loss = [torch.zeros_like(model.loss_G) for _ in range(dist.get_world_size())]
+        dist.all_gather(batch_loss, model.loss_G)
+        loss_list.extend(batch_loss)
+        if pbar is not None:
+            pbar.update(1)
 
-        train_loss_G.add(model.loss_G.item())
+    if pbar is not None:
+        pbar.close()
 
-    return train_loss_G.item()
+    loss = [i.item() for i in loss_list]
+    return mean(loss)
 
 
 def main(config_, save_path, args):
-    global config, log, writer
+    global config, log, writer, log_info
     config = config_
     log, writer = utils.set_save_path(save_path, remove=False)
     with open(os.path.join(save_path, 'config.yaml'), 'w') as f:
@@ -100,9 +158,7 @@ def main(config_, save_path, args):
     model.optimizer = optimizer
     lr_scheduler = CosineAnnealingLR(model.optimizer, config['epoch_max'], eta_min=config.get('lr_min'))
 
-    n_gpus = len(os.environ['CUDA_VISIBLE_DEVICES'].split(','))
     model = model.cuda()
-
     model = torch.nn.parallel.DistributedDataParallel(
         model,
         device_ids=[args.local_rank],
@@ -118,68 +174,66 @@ def main(config_, save_path, args):
     for name, para in model.named_parameters():
         if "image_encoder" in name and "prompt_generator" not in name:
             para.requires_grad_(False)
+    if local_rank == 0:
+        model_total_params = sum(p.numel() for p in model.parameters())
+        model_grad_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print('model_grad_params:' + str(model_grad_params), '\nmodel_total_params:' + str(model_total_params))
 
     epoch_max = config['epoch_max']
     epoch_val = config.get('epoch_val')
     max_val_v = -1e18 if config['eval_type'] != 'ber' else 1e8
-
     timer = utils.Timer()
     for epoch in range(epoch_start, epoch_max + 1):
         train_loader.sampler.set_epoch(epoch)
         t_epoch_start = timer.t()
-        log_info = ['epoch {}/{}'.format(epoch, epoch_max)]
-        writer.add_scalar('lr', optimizer.param_groups[0]['lr'], epoch)
         train_loss_G = train(train_loader, model)
         lr_scheduler.step()
-        log_info.append('train G: loss={:.4f}'.format(train_loss_G))
-        writer.add_scalars('loss', {'train G': train_loss_G}, epoch)
 
-        model_ = model
+        if local_rank == 0:
+            log_info = ['epoch {}/{}'.format(epoch, epoch_max)]
+            writer.add_scalar('lr', optimizer.param_groups[0]['lr'], epoch)
+            log_info.append('train G: loss={:.4f}'.format(train_loss_G))
+            writer.add_scalars('loss', {'train G': train_loss_G}, epoch)
 
-        model_spec = config['model']
-        model_spec['sd'] = model_.state_dict()
-        optimizer_spec = config['optimizer']
-        optimizer_spec['sd'] = optimizer.state_dict()
+            model_spec = config['model']
+            model_spec['sd'] = model.state_dict()
+            optimizer_spec = config['optimizer']
+            optimizer_spec['sd'] = optimizer.state_dict()
 
-        save(config, model, save_path, 'last')
+            save(config, model, save_path, 'last')
 
         if (epoch_val is not None) and (epoch % epoch_val == 0):
-            if n_gpus > 1 and (config.get('eval_bsize') is not None):
-                model_ = model.module
-            else:
-                model_ = model
+            result1, result2, result3, result4, metric1, metric2, metric3, metric4 = eval_psnr(val_loader, model,
+                eval_type=config.get('eval_type'))
 
-            metric1, metric2, metric3, metric4 = eval_psnr(val_loader, model_,
-                data_norm=config['data_norm'],
-                eval_type=config.get('eval_type'),
-                eval_bsize=config.get('eval_bsize'))
+            if local_rank == 0:
+                log_info.append('val: {}={:.4f}'.format(metric1, result1))
+                writer.add_scalars(metric1, {'val': result1}, epoch)
+                log_info.append('val: {}={:.4f}'.format(metric2, result2))
+                writer.add_scalars(metric2, {'val': result2}, epoch)
+                log_info.append('val: {}={:.4f}'.format(metric3, result3))
+                writer.add_scalars(metric3, {'val': result3}, epoch)
+                log_info.append('val: {}={:.4f}'.format(metric4, result4))
+                writer.add_scalars(metric4, {'val': result4}, epoch)
 
-            log_info.append('val: f1={:.4f}'.format(metric1))
-            writer.add_scalars('f1', {'val': metric1}, epoch)
-            log_info.append('val: auc={:.4f}'.format(metric2))
-            writer.add_scalars('auc', {'val': metric2}, epoch)
-            log_info.append('val: metric3={:.4f}'.format(metric3))
-            writer.add_scalars('metric3', {'val': metric3}, epoch)
-            log_info.append('val: metric4={:.4f}'.format(metric4))
-            writer.add_scalars('metric4', {'val': metric4}, epoch)
+                if config['eval_type'] != 'ber':
+                    if result1 > max_val_v:
+                        max_val_v = result1
+                        save(config, model, save_path, 'best')
+                else:
+                    if result3 < max_val_v:
+                        max_val_v = result3
+                        save(config, model, save_path, 'best')
 
-            if config['eval_type'] != 'ber':
-                if metric1 > max_val_v:
-                    max_val_v = metric1
-                    save(config, model, save_path, 'best')
-            else:
-                if metric3 < max_val_v:
-                    max_val_v = metric3
-                    save(config, model, save_path, 'best')
+                t = timer.t()
+                prog = (epoch - epoch_start + 1) / (epoch_max - epoch_start + 1)
+                t_epoch = utils.time_text(t - t_epoch_start)
+                t_elapsed, t_all = utils.time_text(t), utils.time_text(t / prog)
+                log_info.append('{} {}/{}'.format(t_epoch, t_elapsed, t_all))
 
-        t = timer.t()
-        prog = (epoch - epoch_start + 1) / (epoch_max - epoch_start + 1)
-        t_epoch = utils.time_text(t - t_epoch_start)
-        t_elapsed, t_all = utils.time_text(t), utils.time_text(t / prog)
-        log_info.append('{} {}/{}'.format(t_epoch, t_elapsed, t_all))
+                log(', '.join(log_info))
+                writer.flush()
 
-        log(', '.join(log_info))
-        writer.flush()
 
 def save(config, model, save_path, name):
     if config['model']['name'] == 'segformer' or config['model']['name'] == 'setr':
@@ -204,7 +258,8 @@ if __name__ == '__main__':
 
     with open(args.config, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-        print('config loaded.')
+        if local_rank == 0:
+            print('config loaded.')
 
     save_name = args.name
     if save_name is None:
