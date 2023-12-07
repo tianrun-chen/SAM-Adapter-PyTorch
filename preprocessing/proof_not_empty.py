fee446467996eaaa889b8e39a11ec2ddf6f64d5b
Michael Albrecht <albrecht@fortiss.org>
2023-12-07 16:45:40 +0100

    added pct_empty

--- preprocessing/proof_not_empty.py
+++ preprocessing/proof_not_empty.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image 
 import numpy as np
+import random
 
 class Proof:
 
@@ -8,17 +9,28 @@ class Proof:
         self.img_path_ = img_path
         self.mask_path_ = mask_path
 
-    def is_empty_del(self):
+    def is_empty_del(self, pct_empty=0.1):
+        #pcr_empty is the percentage of images that are allowed to be empty
         deleted_images = 0
         total_images = 0
         for mask_filename in os.listdir(self.mask_path_):
             mask_filepath = os.path.join(self.mask_path_, mask_filename)
             mask_png = Image.open(mask_filepath)
             mask_np = np.asarray(mask_png)
+            empty_img = []
             if mask_np.sum() < 20:
-                os.remove(mask_filepath)
-                img_filepath = os.path.join(self.img_path_,mask_filename)
-                os.remove(img_filepath)
-                print(f'deleted image {mask_filename} total deleted images {deleted_images} of {total_images}')
-                deleted_images += 1
+                empty_img.append(mask_filename)
+
             total_images += 1
+
+        for _ in range(int(len(empty_img)*(1-pct_empty))):
+            empty_img.pop(random.randrange(0,len(empty_img)))
+
+        for filename in empty_img:
+            mask_filepath = os.path.join(self.mask_path_,filename)
+            os.remove(mask_filepath)
+            img_filepath = os.path.join(self.img_path_,filename)
+            os.remove(img_filepath)
+            deleted_images += 1
+
+        print(f'total deleted images {deleted_images} of {total_images}')
