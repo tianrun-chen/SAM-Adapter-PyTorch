dd30c37765d99bc339a02d6ea72ed5ec01100eaf
Michael Albrecht <albrecht@fortiss.org>
2023-12-07 15:00:14 +0100

    updated show_false_true to be run with one prompt and without using make_binary before

--- postprocessing/ma_show_results.py
+++ postprocessing/ma_show_results.py
@@ -1,24 +1,26 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
-import ma_make_overlay
 import os
 from statistics import mean
+import argparse
 
-
-def create_overlay_stats(img_path, test_mask_path, pred_mask_path, save_path):
+def create_overlay_stats(img_path, test_mask_path, pred_mask_path, save_path, threshold):
     iou_list = []
-    for img_filename, test_mask_filename, pred_mask_filename in zip(os.listdir(img_path),os.listdir(test_mask_path),os.listdir(pred_mask_path)):
+    for filename in os.listdir(img_path):
+        filename = filename.split('.')[0]
         print(img_path)
-        print(img_filename)
-        image = Image.open(f'{img_path}/{img_filename}')
+        print(filename)
+        image = Image.open(f'{img_path}/{filename}.png')
 
-        test_mask_img = Image.open(f'{test_mask_path}/{test_mask_filename}')
+        test_mask_img = Image.open(f'{test_mask_path}/{filename}.png')
         test_mask = np.asarray(test_mask_img)
 
 
-        pred_mask_img= Image.open(f'{pred_mask_path}/{pred_mask_filename}')
-        pred_mask_img= pred_mask_img.resize((np.shape(test_mask)))
+        pred_mask_np= np.load(f'{pred_mask_path}/{filename}.npy')
+        bin_mask_np = pred_mask_np > threshold
+        bin_mask_img = Image.fromarray(bin_mask_np)
+        pred_mask_img= bin_mask_img.resize((np.shape(test_mask)))
         pred_mask = np.asarray(pred_mask_img)
 
         output = np.zeros(shape=(np.shape(test_mask)[0],np.shape(test_mask)[1],4),dtype=np.uint8)
@@ -64,12 +66,27 @@ def create_overlay_stats(img_path, test_mask_path, pred_mask_path, save_path):
         output_img = Image.fromarray(output,'RGBA')
         image_combined = Image.blend(img_conv,output_img,alpha)
 
-        clean_filename = ma_make_overlay.cleanFilename(img_filename)
 
         plt.imshow(image_combined)
         plt.title(f'yellow: positive, green: false positive, blue: false negative\nPredicted size difference: {np.sum(pred_mask)/np.sum(test_mask)}\nIoU: {true_one/(false_one+false_zero+true_one)}')
-        plt.savefig(f'{save_path}/{clean_filename}.png')
+        plt.savefig(f'{save_path}/{filename}.png')
 
 
 
-create_overlay_stats('load/img/test','load/masks/test','binary','overlay/stats_overlay')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--imagepath', default='load/img/test')
+    parser.add_argument('--groundtruth', default='load/masks/test')
+    parser.add_argument('--prednp', default='test/numpy')
+    parser.add_argument('--savepath', default='overlay/stats_overlay')
+    parser.add_argument('--threshold', default=0.5, type=float)
+
+    args = parser.parse_args()
+
+    image_path = args.imagepath
+    groundtruth = args.groundtruth
+    pred_np = args.prednp
+    save_path = args.savepath
+    threshold = args.threshold
+
+    create_overlay_stats(image_path, groundtruth, pred_np, save_path, threshold)
