9f770e0e9b6ec58bf3484cfe2e25ef141da1f428
Michael Albrecht <albrecht@fortiss.org>
2023-12-11 14:15:59 +0100

    added function to calculate tiles with two coos given

--- preprocessing/download_open_data.py
+++ preprocessing/download_open_data.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from tqdm import tqdm
 
 # in the making
 
@@ -8,8 +9,8 @@ def utm_to_download(x: str, y: str):
 
     zone = 32
 
-    x_tile = x[:3]
-    y_tile = y[:4]
+    x_tile = x[:-3]
+    y_tile = y[:-3]
 
     return f'https://download1.bayernwolke.de/a/dop40/data/{zone}{x_tile}_{y_tile}.tif'
 
@@ -23,11 +24,30 @@ def download_open_data(x: str, y: str, out_dir: str):
     out_file = os.path.join(out_dir, filename)
 
     if not os.path.exists(out_file):
-        print(f'Downloading {url} to {out_file}')
         r = requests.get(url, allow_redirects=True)
-        open(filename, 'wb').write(r.content)
+        open(out_file, 'wb').write(r.content)
     else:
         print(f'File {out_file} already exists, skipping download.')
 
 
-download_open_data('688025', '5330033', '/')
+def calculate_needed_tiles(x1: str, y1: str, x2: str, y2: str):
+    """ Calculates the tiles needed to cover the area of interest. """
+    tiles = []
+
+    # y values
+    for y_val in range(int(y1[:-3]), int(y2[:-3])+1):
+        # x values
+        for x_val in range(int(x1[:-3]), int(x2[:-3])+1):
+            tiles.append((f'{x_val}000', f'{y_val}000'))
+    
+    return tiles
+
+def download_list_of_tiles(tiles: list, out_dir: str):
+    """ Downloads a list of tiles. """
+    for tile in tqdm(tiles, desc='Downloading tiles'):
+        download_open_data(tile[0], tile[1], out_dir)
+
+def calculate_and_download(x1: str, y1: str, x2: str, y2: str, out_dir: str):
+    """ Calculates and downloads all tiles needed to cover the area of interest. """
+    tiles = calculate_needed_tiles(x1, y1, x2, y2)
+    download_list_of_tiles(tiles, out_dir)
