39ea4e852b2761beca1126bfd81b830420df25ca
Michael Albrecht <albrecht@fortiss.org>
2023-12-14 16:50:10 +0100

    changed starting fw_cuda skript to shell command

--- forwardpass/run_forwardpass.py
+++ forwardpass/run_forwardpass.py
@@ -7,23 +7,19 @@ sys.path.append(path)
 from download_open_data import DownloadOpenData
 from split_return import Split
 from save_img_mask import Save
-import fw_cuda
 import argparse
-
-#under construction
 
 def run(lat_1, lon_1, lat_2, lon_2, output_folder):
     download_ = DownloadOpenData()
-    download_.wgs84_download(lat_1, lon_1, lat_2, lon_2, output_folder)
+    download_.wgs84_download(lat_1, lon_1, lat_2, lon_2, f'{output_folder}/tiles_download')
 
     split = Split(2500, 256)
-    images = split.splitImages(output_folder)
+    images = split.splitImages(f'{output_folder}/tiles_download')
 
     save_ = Save()
-    save_.saveImg('forwardpass/data/split_img', images, '')
+    save_.saveImg(f'{output_folder}/split_img', images, '')
 
-    fw_cuda.run_forwardpass('configs/ma_B_cuda_fw.yaml', 'save/_ma_B/dv_29_18/model_epoch_last.pth', 'output')
-
+    os.system(f'python fw_cuda.py --config configs/ma_B_cuda_fw.yaml --model save/_ma_B/dv_29_18/model_epoch_last.pth --output_dir {output_folder}/pred_masks')
 
 
 
@@ -33,7 +29,7 @@ if __name__ == '__main__':
     parser.add_argument('--lon_1', type=float)
     parser.add_argument('--lat_2', type=float)
     parser.add_argument('--lon_2', type=float)
-    parser.add_argument('--output_folder', default='forwardpass/data/tiles_download')
+    parser.add_argument('--output_folder', default='forwardpass/data')
 
     args = parser.parse_args()
 
