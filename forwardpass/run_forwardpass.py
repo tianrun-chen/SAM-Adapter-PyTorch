a2a58c30df3efc0e2a3e12dbb5918da9afdfb367
Michael Albrecht <albrecht@fortiss.org>
2023-12-14 17:19:43 +0100

    added flags to define model, config

--- forwardpass/run_forwardpass.py
+++ forwardpass/run_forwardpass.py
@@ -12,7 +12,7 @@ from save_img_mask import Save
 from ma_make_overlay import *
 import argparse
 
-def run(lat_1, lon_1, lat_2, lon_2, output_folder):
+def run(lat_1, lon_1, lat_2, lon_2, config, model, output_folder):
     download_ = DownloadOpenData()
     download_.wgs84_download(lat_1, lon_1, lat_2, lon_2, f'{output_folder}/tiles_download')
 
@@ -22,7 +22,7 @@ def run(lat_1, lon_1, lat_2, lon_2, output_folder):
     save_ = Save()
     save_.saveImg(f'{output_folder}/split_img', images, '')
 
-    os.system(f'python fw_cuda.py --config configs/ma_B_cuda.yaml --model save/_ma_B/dv_29_18/model_epoch_last.pth --output_dir {output_folder}/pred_masks')
+    os.system(f'python fw_cuda.py --config {config} --model {model} --output_dir {output_folder}/pred_masks')
 
     run_overlay(f'{output_folder}/split_img', f'{output_folder}/pred_masks/png', f'{output_folder}/overlay')
 
@@ -34,7 +34,9 @@ if __name__ == '__main__':
     parser.add_argument('--lon_1', type=float)
     parser.add_argument('--lat_2', type=float)
     parser.add_argument('--lon_2', type=float)
-    parser.add_argument('--output_folder', default='forwardpass/data')
+    parser.add_argument('--config', type=str, default='configs/ma_B_cuda.yaml')
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--output_folder', type=str, default='forwardpass/data')
 
     args = parser.parse_args()
 
@@ -42,6 +44,8 @@ if __name__ == '__main__':
     lon_1 = args.lon_1
     lat_2 = args.lat_2
     lon_2 = args.lon_2
+    config = args.config
+    model = args.model
     output_folder = args.output_folder
 
-    run(lat_1, lon_1, lat_2, lon_2, output_folder)
+    run(lat_1, lon_1, lat_2, lon_2, config, model, output_folder)
