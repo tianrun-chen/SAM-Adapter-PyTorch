c399632019c055df86041b23c3fb9a1dfa74feed
Aleksandre Kandelaki <kandelakialeksandre@gmail.com>
2024-01-16 08:54:22 +0100

    Resolve minor bug

--- train.py
+++ train.py
@@ -175,6 +175,7 @@ if __name__ == '__main__':
     with open(args.config, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
         # Save config
+        os.makedirs(save_path, exist_ok=True)
         save_path = config['write_dir']
         with open(os.path.join(save_path, 'config.yaml'), 'w') as f:
             yaml.dump(config, f)
